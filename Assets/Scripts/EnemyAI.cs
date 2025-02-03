ca05c5e38987b12c1c561dc5b60987065c7c2575
Ryan <120147127+rsuber406@users.noreply.github.com>
2025-02-03 18:50:00 -0500

    adding more AI code

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -9,12 +9,17 @@ public class EnemyAI : MonoBehaviour, IDamage
     [SerializeField] private int health;
 
     [SerializeField] private int mana;
-    [SerializeField] private bool isMelee;
     [SerializeField] private int range;
     // Replace GameObject with the scriptable spell type
     [SerializeField] private List<GameObject> projectiles = new List<GameObject>();
     [Header("Configs")]
+    [SerializeField] private bool isMelee;
+
+    [SerializeField] private bool canRoam;
+    [SerializeField] private int roamDistance;
     [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private int FOV;
+    private float convertedFOV = 0;
     private Vector3 playerPos;
     
     
@@ -24,30 +29,37 @@ public class EnemyAI : MonoBehaviour, IDamage
     
     void Start()
     {
-        
+        playerPos = AIController.GetAIController().GetPlayerPosition();
+        convertedFOV = (float)FOV / 100f;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        CheckPlayerInRange();
     }
 
     void CheckPlayerInRange()
     {
-        if (playerDetected)
+        if (playerDetected && !CanSeePlayer())
         {
             
         }
+        
     }
 
-    void CanSeePlayer()
+    bool CanSeePlayer()
     {
         float dotProduct = Vector3.Dot(transform.forward, (playerPos - transform.position).normalized);
         if (dotProduct > 0.3f)
         {
-            
+            // Needs more logic
+            Debug.Log(dotProduct);
+            return true;
         }
+
+        return false;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -56,6 +68,7 @@ public class EnemyAI : MonoBehaviour, IDamage
 
         if (other.CompareTag("Player"))
         {
+            playerPos = AIController.GetAIController().UpdatePlayerPosition();
             playerDetected = true;
         }
         
