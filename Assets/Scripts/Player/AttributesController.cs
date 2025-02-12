cbcded60a4b61c05556bf02c4179576949a52b45
Gareth Thomas <11306782+forevergareth@users.noreply.github.com>
2025-02-12 00:40:30 -0500

    Added attribute management script for player

--- Assets/Scripts/Player/AttributesController.cs
+++ Assets/Scripts/Player/AttributesController.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class AttributesController : MonoBehaviour
 {
+    public AttributeInfo health;
+    public AttributeInfo mana;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        health.Reset();
+        mana.Reset();
     }
 
     // Update is called once per frame
