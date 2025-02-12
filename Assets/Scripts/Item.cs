b4e0db1d8d41b73cc6cff2d3eb6a784775ff5b8c
DakotaCottman <djcottman@gmail.com>
2025-02-12 16:02:08 -0500

    added getters and a trigger for items
    added some getters and a loaditem data function along with a trigger

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -9,13 +9,33 @@ public class Item : MonoBehaviour
     public ItemData data => itemData;
     public string itemName;
 
-   private void LoadItemData()
+    private void LoadItemData()
     {
         if (itemData != null)
         {
             itemName = itemData.name;
         }
     }
-
-
+    public ItemType GetItemType()
+    {
+        return itemData.itemType;
+    }
+    public string GetDescription()
+    {
+        return itemData.description;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Inventory inventory = other.GetComponent<Inventory>();
+            if (inventory != null)
+            {
+                if (inventory.AddItem(this))
+                {
+                    gameObject.SetActive(false);
+                }
+            }
+        }
+    }
 }
