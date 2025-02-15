5a5dc72c418877575e4c06efeab24e9cb5362b38
Ryan <120147127+rsuber406@users.noreply.github.com>
2025-02-14 19:26:07 -0500

    Spawn controller and Spawner logic is working. You need to feed the spawn controller spawners and then the spawners will take spawn locations

---
+++ Assets/Scripts/SpawningScripts/Spawner/Spawner.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private List<Transform> spawnPoints;
+
+    private List<GameObject> enemyToSpawn;
+
+    private bool containsBoss = false;
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.isTrigger) return;
+        
+        SpawnEnemy();
+        
+    }
+    public void ChosenEnemy(List<GameObject> enemies)
+    {
+        enemyToSpawn = enemies;
+        containsBoss = true;
+    }
+
+    private void SpawnEnemy()
+    {
+        for (int i = 0; i < spawnPoints.Count; i++)
+        {
+            if (containsBoss)
+            {
+                Instantiate(enemyToSpawn[0], spawnPoints[i].position, Quaternion.identity);
+                break;
+            }
+            else
+            {
+                int randomEnemy = Random.Range(0, enemyToSpawn.Count);
+                enemyToSpawn[randomEnemy].transform.position = spawnPoints[i].position;
+                // Replace this with object pooling, get it to work and go from there
+                Instantiate(enemyToSpawn[randomEnemy], spawnPoints[i].position, Quaternion.identity);
+            }
+        }
+    }
+    
+    
+}
