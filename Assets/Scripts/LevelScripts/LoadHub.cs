a73df18c7e398f1eb5c3fdd37bdebfed692a4851
AV-Cables <ajmcdo618@gmail.com>
2025-02-16 19:59:04 -0500

    Scene Transitions Completed. Hub and Linear Scene transitions are implemented

--- Assets/Scripts/LevelScripts/LoadHub.cs
+++ Assets/Scripts/LevelScripts/LoadHub.cs
@@ -5,9 +5,8 @@ public class loadHub : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            //get rid of current map at 0,0,0. It will never be Hub
-            MainSceneLogic.MSInstance.loadHub();
             //Move Player to 0, 0, -32.20
+            MainSceneLogic.MSInstance.mapnum = 0;
             //GameManager.instance.manaBar;
             GameManager.instance.TeleportPlayer(0f, 0f, -32.20f);
         }
