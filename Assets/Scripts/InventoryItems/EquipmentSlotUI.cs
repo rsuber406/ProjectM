cbb8ab8f44f146ca07266fe4030bb9caf19ad823
DakotaCottman <djcottman@gmail.com>
2025-02-18 16:46:56 -0500

    equipment slot added
    equipment slot added, can transfer from inventory to equipmentui.

---
+++ Assets/Scripts/InventoryItems/EquipmentSlotUI.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class EquipmentSlotUI : MonoBehaviour, IPointerClickHandler, IDragHandler, IBeginDragHandler, IEndDragHandler, IDropHandler
+{
+    public ArmorType armorType;
+    private EquipmentManager equipmentManager;
+    private Image imageComponent;
+    private CanvasGroup canvasGroup;
+    private Vector2 originalPos;
+    private bool isDraggingItem = false;
+    private RectTransform rectTransform;
+
+
+    private void Awake()
+    {
+        equipmentManager = FindAnyObjectByType<EquipmentManager>();
+        imageComponent = GetComponent<Image>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (imageComponent.sprite != null && !isDraggingItem)
+        {
+            isDraggingItem = true;
+            originalPos = rectTransform.anchoredPosition;
+            canvasGroup.alpha = 0.6f;
+            canvasGroup.blocksRaycasts = false;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (isDraggingItem)
+        {
+            rectTransform.position = eventData.position;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (isDraggingItem)
+        {
+            ResetDragState();
+        }
+    }
+    private void ResetDragState()
+    {
+        isDraggingItem = false;
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+        rectTransform.anchoredPosition = originalPos;
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        InventorySlotUI fromSlot = eventData.pointerDrag?.GetComponent<InventorySlotUI>();
+
+        if (fromSlot != null) 
+        {
+            ItemData itemData = fromSlot.GetComponent<InventorySlotUI>().inventory.slots[fromSlot.slotIndex].item.data; 
+
+            if (itemData.itemType == ItemType.Armor)
+            {
+               
+                fromSlot.inventory.RemoveItem(itemData.itemName, fromSlot.slotIndex);
+                equipmentManager.EquipItem(itemData);
+                imageComponent.sprite = itemData.icon;
+               
+            }
+        }
+    }
+}
