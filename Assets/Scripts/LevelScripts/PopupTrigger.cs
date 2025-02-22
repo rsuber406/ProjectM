df28633b445b7e9c58a90f62fa903559fb134638
AV-Cables <ajmcdo618@gmail.com>
2025-02-22 15:19:19 -0500

    Tutorial popups working

--- Assets/Scripts/LevelScripts/PopupTrigger.cs
+++ Assets/Scripts/LevelScripts/PopupTrigger.cs
@@ -1,14 +1,46 @@
 using UnityEngine;
+using TMPro;
+using System.Collections;
 
 public class PopupTrigger : MonoBehaviour
 {
+    public GameObject popupPanel;
+    public TextMeshProUGUI popupText;
+
+    private Coroutine mainCoroutine;
     public int messageIndex;
+
+
+    private string[] messages = {
+        "Test1",
+        "Test2",
+        "Test3",
+        "Test4",
+        "Test5"
+    };
+
+    public void ShowPopup(int messageIndex, float duration = 4f)
+    {
+        popupText.text = messages[messageIndex];
+        popupPanel.SetActive(true);
+
+        //StopCoroutine(mainCoroutine);
+
+        mainCoroutine = StartCoroutine(HidePopup(duration));
+
+    }
+
+    private IEnumerator HidePopup(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        popupPanel.SetActive(false);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            PopupManager.ShowPopup(messageIndex, 4f);
+            ShowPopup(messageIndex, 4f);
         }
     }
 }
