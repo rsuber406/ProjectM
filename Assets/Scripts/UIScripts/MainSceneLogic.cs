2caf80cb1872149e7373e66a663b4c142a90938c
Ryan Suber <rsuber406@gmail.com>
2025-02-23 12:11:39 -0500

    Persistent save re-implemented according to new logic for main menu

--- Assets/Scripts/UIScripts/MainSceneLogic.cs
+++ Assets/Scripts/UIScripts/MainSceneLogic.cs
@@ -174,6 +174,7 @@ public class MainSceneLogic : MonoBehaviour
     public void Quitgame()
     {
         GameManager.GetInstance().GetSoundManager().MenuClick(1);
+        GameManager.GetInstance().SavePlayerData();
 
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
