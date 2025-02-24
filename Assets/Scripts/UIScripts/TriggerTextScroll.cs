af36c4a29ac838c80b4f14d22baf28e59c3910f5
AV-Cables <ajmcdo618@gmail.com>
2025-02-23 21:18:25 -0500

    Credits Working pt 2

--- Assets/Scripts/UIScripts/TriggerTextScroll.cs
+++ Assets/Scripts/UIScripts/TriggerTextScroll.cs
@@ -1,11 +1,48 @@
+using System.Collections;
 using UnityEngine;
 
 public class ScrollTextTrigger : MonoBehaviour
 {
-    public Animator textAnimator;
 
-    public void PlayScrollAnimation()
+    public GameObject TexttoScroll;
+    bool ismoving = false;
+    bool startmoving = false;
+    Transform originalPosition;
+
+    private void Start()
     {
-        textAnimator.SetTrigger("StartScroll");
+        originalPosition = TexttoScroll.transform;
+    }
+    private void Update()
+    {
+        if(!ismoving && startmoving)
+        {
+            StartCoroutine(moveText());
+        }
+
+        if (!startmoving)
+        {
+            TexttoScroll.transform.position = originalPosition.position;
+        }
+    }
+
+    IEnumerator moveText()
+    {
+        ismoving = true;
+        TexttoScroll.transform.position += new Vector3(0, 1f, 0);
+        ismoving = false;
+        yield return new WaitForSeconds(0.13f);
+    }
+
+    public void StartScrolling()
+    {
+        if (startmoving)
+        {
+            startmoving = false;
+        }
+        else
+        {
+            startmoving = true;
+        }    
     }
 }
