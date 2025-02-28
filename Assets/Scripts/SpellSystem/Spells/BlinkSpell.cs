ea99bda260f32b20061382bd6e2029ace1123bcd
Ryan <120147127+rsuber406@users.noreply.github.com>
2025-02-28 10:49:00 -0500

    Temp fix for blink, needs a little bit more work but its almost there

--- Assets/Scripts/SpellSystem/Spells/BlinkSpell.cs
+++ Assets/Scripts/SpellSystem/Spells/BlinkSpell.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "BlinkSpell", menuName = "Spell System/ Spells /BlinkSpell")]
 public class BlinkSpell : SpellBase
 {
-    [Header("Spell Properties")]
-    public float Distance;
+    [Header("Spell Properties")] public float Distance;
 
     public AudioClip CastAudioClip;
-    [Range(0,1)] public float CastAudioPitch;
+    [Range(0, 1)] public float CastAudioPitch;
     public GameObject ParticleEffects;
     private GameObject effect;
 
@@ -30,28 +29,55 @@ public class BlinkSpell : SpellBase
         GameObject player = GameManager.GetInstance().GetPlayer();
         Rigidbody playerRigidBody = player.GetComponent<Rigidbody>();
         AudioSource playerAudioSource = player.GetComponent<AudioSource>();
-
+        PlayerController playerControllerRef = player.GetComponent<PlayerController>();
 
         // Blink in the cameras forward direction by default
         Transform cameraTransform = GameManager.GetInstance().GetPlayerCamera().transform;
         Vector3 direction = cameraTransform.forward;
 
-        if (playerRigidBody.linearVelocity.magnitude > 0.13) 
+        if (playerRigidBody.linearVelocity.magnitude > 0.13)
             direction = playerRigidBody.linearVelocity.normalized;
-        
-        direction = Vector3.ProjectOnPlane(direction, Vector3.up).normalized;
-        
-        playerRigidBody.AddForce(direction * Distance, ForceMode.Impulse);
-        player.transform.GetChild(0).rotation = Quaternion.Euler(0, cameraTransform.eulerAngles.y, 0);
-        
+       
+        Vector3 adjustedDirection = playerControllerRef.HandSocket.forward;
+        if (playerRigidBody.velocity.z < 0)
+        {
+            Quaternion adjustRotation = Quaternion.Euler(-15, 0, 0);
+            adjustedDirection = adjustRotation * adjustedDirection;
+        }
+        else
+        {
+            Quaternion adjustRotation = Quaternion.Euler(15, 0, 0);
+            adjustedDirection = adjustRotation * adjustedDirection;
+        }
+        RaycastHit hit;
+        int ignoreLayer = LayerMask.GetMask("Player");
+        if (Physics.Raycast(playerControllerRef.HandSocket.position, adjustedDirection, out hit, 10f, ~ignoreLayer))
+        {
+            if (hit.collider.CompareTag("Stair"))
+            {
+                Vector3 playerNewPosition = new Vector3(hit.point.x, hit.collider.bounds.max.y, hit.point.z);
+                player.transform.position = playerNewPosition;
+                playerRigidBody.position = playerNewPosition;
+                
+            }
+            else
+            {
+                player.transform.position = hit.point;
+                playerRigidBody.position = hit.point;
+            }
+        }
+
+        // playerRigidBody.AddForce(direction * Distance, ForceMode.Impulse);
+        // player.transform.GetChild(0).rotation = Quaternion.Euler(0, cameraTransform.eulerAngles.y, 0);
+
         float sfxVolume = GameManager.GetInstance().GetSoundManager().audSFX.volume;
         playerAudioSource.PlayOneShot(CastAudioClip, CastAudioPitch * sfxVolume);
-        
+
         if (ParticleEffects)
         {
             effect = Instantiate(ParticleEffects, player.transform);
         }
-        
+
         // Cast time , for som reason this is needed before we can call end
         yield return new WaitForSeconds(1);
         End();
